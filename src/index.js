98d82dfe693a5e91cb6a4d835523bf54d6d45327
Daniel Martínez <mmjosedaniel@hotmail.com>
2020-08-30 15:15:53 -0500

    Showing the Past Moves

--- src/index.js
+++ src/index.js
@@ -77,6 +77,17 @@ import './index.css';
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
       let status;
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
   
       if (winner) {
         status = 'Winner: ' + winner;
@@ -94,7 +105,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
